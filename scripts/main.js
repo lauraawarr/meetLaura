07ac26ff8318b351c042dbab29159329e7994f96
Laura Warr <lauraawarr@gmail.com>
2017-01-26 21:55:19 -0500

    Added typing animation on load

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', onLoad);
 
 function onLoad(){
+
 	$('html').animate({scrollTop:0}, 1);
     $('body').animate({scrollTop:0}, 1);
 
@@ -9,7 +10,46 @@ function onLoad(){
 	setTimeout(function(){
 		document.querySelector('body').removeChild(loading);
 	},800);
+
+	var heightMatch = window.matchMedia("(min-height: 600px)").matches;
+	if (heightMatch) {
+
+		var strArray = [ 
+			"making beautiful websites.", 
+			"elegant code.", 
+			"javascript.",
+			"a good challenge."
+			];
+		var string = strArray[Math.floor(strArray.length*Math.random())];
+		setTimeout(function(){
+			type(string)
+		}, 800);
+	};
 };
+
+var heightMatch = window.matchMedia("(min-height: 600px)").matches;
+if (heightMatch) {
+
+	var writing = document.querySelector('#writing');
+	writing.style.opacity = "1";
+	writing.querySelector('.prefix').innerHTML = "I love ";
+
+	var output = document.querySelector('#writing .output');
+	output.innerHTML = "";
+
+	function type(string){
+		var i = 0;
+		var interval = setInterval(function(){
+			output.innerHTML += string[i];
+			i++;
+			if (i == string.length) clearInterval(interval);
+		}, 100);
+	};
+} else {
+	var subheader = document.querySelector('#subheader');
+	subheader.style.opacity = "1";
+}
+
 
 //// Google Analytics //////
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
